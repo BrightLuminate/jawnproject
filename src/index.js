cdb718c19082254da5a6e32a78a0c5b0df486c0a
BrightLuminate <grace71394@gmail.com>
2025-06-05 11:22:44 +0900

    Fix module import paths with .js extension for ESM compatibility

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './index.css'; // CSS 파일은 확장자 생략 가능 (Webpack이 처리)
+import App from './App.js'; // <-- 이전에 수정했을 것
+import reportWebVitals from './reportWebVitals.js'; // <-- 이 부분을 수정합니다!
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -15,3 +15,5 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+

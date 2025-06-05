0d70d677bd2fbf2d13e5fbd672be35dc9fafde6f
BrightLuminate <159859713+BrightLuminate@users.noreply.github.com>
2025-06-05 11:53:19 +0900

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,6 +4,8 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSitemap,  faBars, faTimes } from '@fortawesome/free-solid-svg-icons';
 import { Link } from 'react-router-dom';
 import logo from '../images/hegun copy.png';
+// The problematic line 'import Navbar from './components/Navbar.js';' has been removed
+// because this file itself defines the Navbar component.
 
 const Navbar = () => {
     const [activeDropdown, setActiveDropdown] = useState(null);
@@ -46,7 +48,6 @@ const Navbar = () => {
                 <Link to="/sitemap" className='logins'>
                     <FontAwesomeIcon icon={faSitemap} /> 사이트맵
                 </Link>
-            
             </div>
 
             <div className='navbar-container'>
@@ -66,33 +67,19 @@ const Navbar = () => {
                     <DropdownMenu index={2} title="소개" items={[
                         { name: '인사말', path: '/greeting' },
                         { name: '연혁', path: '/history' },
-                       
                     ]} />
-
 
                     <DropdownMenu index={3} title="참여" items={[
                        { name: '교수님과의 대화', path: '/Communication' },
                         { name: '국악신문사', path: '/gugak-newspaper' }
                     ]} />
 
-
-
-{/* 
-
-                    <DropdownMenu index={3} title="교육 활동" items={[
-                        { name: '교육 문의', path: '/education-inquiry' },
-                        { name: '단체 문의', path: '/group-inquiry' }
-                    ]} /> */}
-
                     <DropdownMenu index={4} title="국악찬양단" items={[
                         { name: '국악찬양단 소개', path: '/gugak-choir-introduction' },
-                      
                     ]} />
-                         <DropdownMenu index={5} title="김영재류 산조보존회" items={[
-                    
+                    <DropdownMenu index={5} title="김영재류 산조보존회" items={[
                         { name: '김영재류 산조보존회 소개', path: '/kim-young-jae-introduction' }
                     ]} />
-
                 </div>
 
                 {/* 모바일 화면에서만 햄버거 버튼 표시 */}
